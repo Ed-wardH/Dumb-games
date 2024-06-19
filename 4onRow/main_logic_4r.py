b8af04ea84a3564147102052d94cdef53b05446d
Ed-wardh <eduardohuza@gmail.com>
2024-06-19 20:40:50 +0200

    Basic playability COMPLETE

--- 4onRow/main_logic_4r.py
+++ 4onRow/main_logic_4r.py
@@ -31,7 +31,15 @@ def showBoard():
     line=""
     for x in board:
         for y in x:
-           line=line+str(y)+" "
+            if(y==2):
+               line=line+"| "
+            else:
+                if(y==0):
+                    p="X"
+                else:
+                    p="O"
+                line=line+"|"+str(p)
+        line=line+"|"    
         print(line)
         line=""
     return 0
@@ -39,22 +47,28 @@ def showBoard():
 # =====\ resetBoard() \=====
 # 1- 
 def resetBoard():
-    for x in range(height+1):
-        for y in range(width+1):
-           board[x][y]=8
+    for x in range(ind_height+1):
+        for y in range(ind_width+1):
+           board[x][y]=2
 
 # =====\ playTurn(playerCode) \=====
 # 1- ask for colum to play until its able
 # 2- set the player move on the lowest row of that colum
 # 2.1- to set the row we take height value and subtract the checkHight value
 def playTurn(player):
-    col=int(input("Select colum: "))
+    col=input("Select colum: ")
+    while(notAbleMove(col)):
+       col=input("Select colum: ")
+    able_col=int(col)
     #col=3
-    row=checkHeight(col)
+    #print(able_col)
+    row=checkHeight(able_col)
     if(row<0):
         playTurn(player)
     else:
-        board[height-row][col] = player
+        board[ind_height-row][able_col] = player
+        #Check WIN
+        checkH(able_col)
         
         return 0
 
@@ -66,6 +80,7 @@ def playTurn(player):
 # 2- set the next able position
 # 3- if position is 6 the colum is full is not able
 def checkHeight(col):
+    #print(type(col))
     solution=col_height[col] 
     if(solution==6):
         #print(-1)
@@ -75,14 +90,82 @@ def checkHeight(col):
         #print(solution)
         return solution
     
-def checkWin():
+# =====|  checkAbleMove(col)  |=====
+def notAbleMove(col):
+    # Comprobation of good imput
+    if(not col.isdigit()):
+        return True
+    if(0<=int(col)<6):
+        return False
+    return True
     
+# Do all posible check wins
+def checkWin(lastcolumPLay):
+    checkH(lastcolumPLay)
+    checkV(lastcolumPLay)
+    checkLR(lastcolumPLay)
+    checkRL(lastcolumPLay)
+    return 0
+
+# Do check win vertical
+# -- pre-check at least 4 colums with same or more height level
+# -- full-check 
+def checkH(col):
+    # get the actual height of the move for the pre-check
+    actualHeight= col_height[col]
+    # get the amount of posible values (must be 4 or grater)
+    count=0
+    for x in col_height:
+        if x>=actualHeight:
+            count+=1
+    # check if the count is enought to go into full-check
+    if(count>=4):
+        # do full-check
+        consecutive_player=1
+        pWinRow=ind_height-actualHeight
+        actual_player=board[pWinRow][col]
+        #go Left
+        i=1
+        while(pWinRow>=0):
+            if(board[pWinRow][col-i]==actual_player):
+                consecutive_player+=1
+            else:
+                continue
+        #go Right
+        i=1
+        while(pWinRow<=6):
+            if(board[pWinRow][col+i]==actual_player):
+                consecutive_player+=1
+            else:
+                continue
+        if(consecutive_player>3):
+            print("WIIIN")
+            showBoard()
+            
+
+        print('posible victoria de :' +str(actual_player))
+    return 0
+
+# Do check win vertical
+#
+def checkV():
+    return 0
+
+# Do check diagonal left to right
+#
+def checkLR():
+    return 0
+
+# Do check diagonla right to left
+#
+def checkRL():
     return 0
 
 
 # EXECUTE PROGRAM
 #showBoard()
 #checkHeight(0)
+#print(checkAbleMove("8"))
 #playTurn(8)
 #resetBoard()
 #showBoard()
@@ -90,12 +173,13 @@ def checkWin():
 #=============================================
 #       RUN GAME
 #=============================================
-def runGame():
+def runGame(n):
+    print("In this game u select a colum to play betwen 0-6")
     resetBoard()
     showBoard()
-    for i in range(4):
+    for i in range(n):
         playTurn(i%2)
         showBoard()
     print("WIP")
 
-runGame()
+runGame(10)
